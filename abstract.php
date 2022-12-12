77717b027fedb6e50d5b9ed708fa228f44712191
Alex Lednev <avlednev@vgtrk.com>
2022-12-12 11:06:37 +0300

    abstract start

---
+++ abstract.php
@@ -0,0 +1,37 @@
+<?php
+
+class FirstThousandPionts
+{
+    public function name()
+    {
+        return 'First Thousands Points';
+    }
+
+    public function icon()
+    {
+        return 'first-thousands-points.png';
+    }
+
+    public function qualifier($user)
+    {
+        
+    }
+}
+
+class FirstBestAnswer
+{
+    public function name()
+    {
+        return 'First Best Answer';
+    }
+
+    public function icon()
+    {
+        return 'first-best-answer.png';
+    }
+
+    public function qualifier($user)
+    {
+
+    }
+}
