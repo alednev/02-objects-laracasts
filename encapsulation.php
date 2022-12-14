0403df8d763a5ece3dd172a7888b4c441d7b5604
Alex Lednev <avlednev@vgtrk.com>
2022-12-14 23:31:52 +0300

    encapsulation

---
+++ encapsulation.php
@@ -0,0 +1,33 @@
+<?php
+
+class TennisMatch
+{
+    protected $playerOne;
+
+    public function score()
+    {
+        // is there a winner
+        // does someone have and advantage
+        // are they in deuce
+    }
+
+    protected function hasWinner()
+    {
+
+    }
+
+    protected function hasAgvantage()
+    {
+
+    }
+
+    protected function inDeuce
+    {
+
+    }
+
+    public function playerOne()
+    {
+        return $this->playerOne;
+    }
+}
