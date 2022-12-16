3744d986ad5b204fc1407f5b38d1225b55418dc3
Alex Lednev <avlednev@vgtrk.com>
2022-12-16 11:45:24 +0300

    custom exception

--- exceptions.php
+++ exceptions.php
@@ -1,34 +1,56 @@
 <?php
 
-class Video
+class MaximumMembersReached extends Exception
 {
-
+    protected $message = 'You can not add more than 3 team members';
 }
 
-class User
+class Member
 {
-    public function download(Video $video)
+    public string $name;
+
+    public function __construct($name)
     {
-        if (! $this->subscribed()) {
-            throw new Exception('You must be subscribed to download videos');
+        $this->name = $name;
+    }
+}
+
+class Team
+{
+    protected $members = [];
+
+    public function add(Member $member)
+    {
+        if (count($this->members)) {
+            throw new MaximumMembersReached();
         }
+        $this->members[] = $member;
     }
 
-    public function subscribed()
+    public function members()
+    : array
     {
-        return false;
+        return $this->members;
     }
 }
 
-class UserDownloadController
+class TeamMemberController
 {
-    public function show()
+    public function store()
     {
+        $team = new Team; // has a maximum of three members
+
         try {
-            (new User)->download(new Video);
-        } catch (Exception $e) {
-            echo $e->getMessage();
+            $team->add(new Member('Jane Doe'));
+            $team->add(new Member('John Doe'));
+            $team->add(new Member('Frank Doe'));
+            $team->add(new Member('Susan Doe'));
+
+            var_dump($team->members());
+        } catch (MaximumMembersReached $e) {
+            var_dump($e->getMessage());
         }
     }
 }
 
+(new TeamMemberController())->store();
